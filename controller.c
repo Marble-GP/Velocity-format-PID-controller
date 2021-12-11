1bbb1a4eeb9fc54a0c9e061427a7f526f77a2df5
Marble-GP <74084344+Marble-GP@users.noreply.github.com>
2021-12-11 15:15:36 +0900

    Add files via upload

---
+++ controller.c
@@ -0,0 +1,156 @@
+/**
+ * @file controller.c
+ * @author Watanabe Shohei
+ * @brief velocity format PID controller module
+ * @version 0.3
+ * @date 2021-11-20
+ * 
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include "controller.h"
+
+
+void PID_Controller_Init(PID_Controller* controller, float KP, float KI, float KD, float ref_lim, float f_cut, float fs)
+{
+    controller->KP = KP;
+    controller->KI = KI;
+    controller->KD = KD;
+    controller->ref_lim = __MYABS(ref_lim);
+    controller->Ts = __MYABS(1.0f/fs);
+    controller->Tc = 1.0f/((float)__MYTWOPI*f_cut);
+
+    controller->__coeff_a = controller->Tc/(controller->Tc+controller->Ts);
+    controller->__coeff_b = controller->Ts / (controller->Tc + controller->Ts);
+
+    controller->__index_count = 0;
+
+    controller->__Pout = 0.0f;
+    controller->__Iout = 0.0f;
+    controller->__Dout[0] = 0.0f;
+    controller->__Dout[1] = 0.0f;
+    controller->__input[0] = 0.0f;
+    controller->__input[1] = 0.0f;
+    controller->__output_pre = 0.0f;
+}
+
+void PID_Controller_Init_std(PID_Controller* controller, float KP, float TI, float TD, float ref_lim, float f_cut, float fs)
+{
+    controller->KP = KP;
+    controller->KI = KP/TI;
+    controller->KD = TD*KP;
+    controller->ref_lim = __MYABS(ref_lim);
+    controller->Tc = 1.0f / ((float)__MYTWOPI * f_cut);
+    controller->Ts = __MYABS(1.0f/fs);
+
+
+    controller->__coeff_a = controller->Tc / (controller->Tc + controller->Ts);
+    controller->__coeff_b = controller->Ts / (controller->Tc + controller->Ts);
+
+    controller->__index_count = 0;
+
+    controller->__Pout = 0.0f;
+    controller->__Iout = 0.0f;
+    controller->__Dout[0] = 0.0f;
+    controller->__Dout[1] = 0.0f;
+    controller->__input[0] = 0.0f;
+    controller->__input[1] = 0.0f;
+    controller->__output_pre = 0.0f;
+}
+
+void PI_Controller_Init(PID_Controller* controller, float KP, float KI, float ref_lim, float f_cut, float fs)
+{
+    controller->KP = KP;
+    controller->KI = KI;
+    controller->KD = 0.0f;
+    controller->ref_lim = __MYABS(ref_lim);
+    controller->Tc = 1.0f / ((float)__MYTWOPI * f_cut);
+    controller->Ts = __MYABS(1.0f / fs);
+
+
+    controller->__coeff_a = controller->Tc / (controller->Tc + controller->Ts);
+    controller->__coeff_b = controller->Ts / (controller->Tc + controller->Ts);
+
+    controller->__index_count = 0;
+
+    controller->__Pout = 0.0f;
+    controller->__Iout = 0.0f;
+    controller->__Dout[0] = 0.0f;
+    controller->__Dout[1] = 0.0f;
+    controller->__input[0] = 0.0f;
+    controller->__input[1] = 0.0f;
+    controller->__output_pre = 0.0f;
+}
+
+void PI_Controller_Init_std(PID_Controller* controller, float KP, float TI, float ref_lim, float f_cut, float fs)
+{
+    controller->KP = KP;
+    controller->KI = KP/TI;
+    controller->KD = 0.0f;
+    controller->ref_lim = __MYABS(ref_lim);
+    controller->Tc = 1.0f / ((float)__MYTWOPI * f_cut);
+    controller->Ts = __MYABS(1.0f/fs);
+
+
+    controller->__coeff_a = controller->Tc / (controller->Tc + controller->Ts);
+    controller->__coeff_b = controller->Ts / (controller->Tc + controller->Ts);
+
+    controller->__index_count = 0;
+
+    controller->__Pout = 0.0f;
+    controller->__Iout = 0.0f;
+    controller->__Dout[0] = 0.0f;
+    controller->__Dout[1] = 0.0f;
+    controller->__input[0] = 0.0f;
+    controller->__input[1] = 0.0f;
+    controller->__output_pre = 0.0f;
+}
+
+
+float PID_Controller_Calculate(PID_Controller* controller, float err_input)
+{
+    controller->__Pout = 
+    controller->KP*(err_input - controller->__input[controller->__index_count^1])/(controller->Ts+controller->Tc)
+     + controller->__coeff_a*controller->__Pout;//update Pout
+
+    controller->__Iout = controller->KI*err_input;//update Iout
+
+    controller->__Dout[controller->__index_count] = controller->KD*(err_input - 2.0f*controller->__input[controller->__index_count^1] + controller->__input[controller->__index_count]) / (controller->Ts + controller->Tc) / (controller->Ts + controller->Tc)
+        + 2.0f*controller->__coeff_b*controller->__Dout[controller->__index_count^1] - controller->__Dout[controller->__index_count]*controller->__coeff_a * controller->__coeff_a;
+    //update Dout[index]
+
+    controller->__index_count ^= 1;// update 0/1 counter
+    controller->__input[controller->__index_count] = err_input;//update previous input
+    //return d(P+I+D)+output_pre
+    return controller->__output_pre = __MYLIMIT((controller->__Pout + controller->__Iout + controller->__Dout[controller->__index_count^1])*controller->Ts + controller->__output_pre, controller->ref_lim);
+}
+
+float PI_Controller_Calculate(PID_Controller* controller, float err_input)
+{
+    controller->__Pout =
+        controller->KP * (err_input - controller->__input[0]) / (controller->Ts + controller->Tc)
+        + controller->__coeff_a * controller->__Pout;//update Pout
+
+    controller->__Iout = controller->KI * err_input;//update Iout
+
+    controller->__input[0] = err_input;//update previous input
+    //return d(P+I)+output_pre
+    return controller->__output_pre = __MYLIMIT((controller->__Pout + controller->__Iout)*controller->Ts + controller->__output_pre, controller->ref_lim);
+}
+
+float PI_Controller_Calculate_nonfilter(PID_Controller* controller, float err_input)
+{
+        controller->__Pout =
+        controller->KP * (err_input - controller->__input[0]) /controller->Ts;
+        controller->__Iout = controller->KI * err_input;
+        
+        controller->__input[0] = err_input;//update previous input
+    return controller->__output_pre = __MYLIMIT((controller->__Pout + controller->__Iout)*controller->Ts + controller->__output_pre, controller->ref_lim);
+}
+
+int __comp(float* p1, float* p2)
+{
+    return *p1 - *p2;
+}
+
---
+++ controller.h
@@ -0,0 +1,86 @@
+/**
+ * @file controller.c
+ * @author Watanabe Shohei
+ * @brief velocity format PID controller module
+ * @version 0.2
+ * @date 2021-09-20
+ * 
+ */
+
+#ifndef __CONTROLLER_H__
+#define __CONTROLLER_H__
+
+#define __MYABS(x) ((x) > 0 ? (x) : (-x))
+#define __MYLIMIT(x, lim) ((x) > (lim) ? (lim) : ((x) < -(lim) ? -(lim) : (x)))
+#define __MYLIMIT2(x, min, max) ((x) > (max) ? (max) : ((x) < (min) ? (min) : (x)))
+#define LIN_MAP(x, in_min, in_max, out_min, out_max) ((out_min) + ((in_val) - (in_min))/((in_max) - (in_min))*((out_max) - (out_min)))
+#define __MYPI    (3.141592653589793)
+#define __MYTWOPI (6.283185307179586)
+
+typedef struct __PID_Controller
+{
+    float KP;
+    float KI;
+    float KD;
+    float ref_lim;
+    float Tc;
+    float Ts;
+    float __coeff_a;// Tc/(Tc+Ts)
+    float __coeff_b;// Ts/(Tc+Ts)
+    float __input[2];
+    float __Iout;
+    float __Pout;
+    float __Dout[2];
+    float __output_pre;
+    uint8_t __index_count;
+    
+
+} PID_Controller;
+
+
+/**
+ * @brief initialize a PID controller
+ * 
+ * @param controller pointer to a controller
+ * @param KP Propotial gain
+ * @param KI Integral gain
+ * @param KD Defferential gain
+ * @param ref_lim limit value of output
+ * @param f_cutoff cut-off freq.[Hz] of each LPF
+ * @param fs sampling(+calclation) freq. [Hz]
+ */
+void PID_Controller_Init(PID_Controller* controller, float KP, float KI, float KD, float ref_lim, float f_cutoff, float fs);
+
+
+/**
+ * @brief initialize a PID controller(standard form)
+ * 
+ * @param controller pointer to a controller
+ * @param KP Propotial gain
+ * @param TI Integral time
+ * @param TD Defferential time
+ * @param ref_lim limit value of output
+ * @param f_cutoff cut-off freq.[Hz] of each LPF
+ * @param fs sampling(+calclation) freq. [Hz]
+ */
+void PID_Controller_Init_std(PID_Controller* controller, float KP, float TI, float TD, float ref_lim, float f_cutoff, float fs);
+
+/**
+ * @brief calculate manipulating value
+ * 
+ * @param controller pointer to the controller
+ * @param val_ref reference value
+ * @param val feedback value
+ * @return float  manipulating value
+ */
+float PID_Controller_Calculate(PID_Controller* controller, float err_input);
+
+
+void PI_Controller_Init(PID_Controller* controller, float KP, float KI, float ref_lim, float f_cut, float fs);
+void PI_Controller_Init_std(PID_Controller* controller, float KP, float TI, float ref_lim, float f_cut, float fs);
+float PI_Controller_Calculate(PID_Controller* controller, float err_input);
+float PI_Controller_Calculate_nonfilter(PID_Controller* controller, float err_input);
+
+int __comp(float* p1, float* p2);
+
+#endif
