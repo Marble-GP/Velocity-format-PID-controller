cfa2a497d1a53db2cf0315aacad531746373bc45
S.Watanabe <74084344+Marble-GP@users.noreply.github.com>
2023-06-29 21:42:44 +0900

    Add files via upload

--- controller.h
+++ controller.h
@@ -2,8 +2,8 @@
  * @file controller.c
  * @author Watanabe Shohei
  * @brief velocity format PID controller module
- * @version 0.2
- * @date 2021-09-20
+ * @version 0.3.1
+ * @date 2023-06-29
  * 
  */
 
